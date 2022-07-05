fecf5886285d3dc73c0c4e11e9d473c287ce7f24
juningwei <289704828@qq.com>
2022-07-05 10:16:06 +0800

    引入elemtent-ui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import { Button, Select } from 'element-ui'
 Vue.config.productionTip = false
+
+Vue.use(Button)
+Vue.use(Select)
 
 new Vue({
   router,
