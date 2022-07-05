a9f9fb9642516b47f19fb6f22497c5ae0433befb
juningwei <289704828@qq.com>
2022-07-05 12:16:39 +0800

    添加登录路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import LoginC from '../components/LoginC'
 Vue.use(VueRouter)
 
-const routes = [
-
-]
-
 const router = new VueRouter({
-  routes
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: LoginC }
+  ]
 })
 
 export default router
